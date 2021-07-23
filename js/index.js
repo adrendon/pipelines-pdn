281dcfd65f2773955a20ae343f18f83622f2b548
arendon <arendon@bancolombia.com.co>
2021-07-23 09:12:19 -0500

    ajuste

--- js/index.js
+++ js/index.js
@@ -43,7 +43,22 @@ riot.tag('todo', '<div class="add" onclick="{ showAdd }"><i class="fa fa-plus"><
 
 riot.mount('todo', {
   title: "Tareas - Paso a Producción - Pipelines",
-  items: []
+  items: [
+    { title: 'Validación de pruebas de performance en Jmeter de manera local', done: false },
+    { title: 'Validación de pruebas de aceptación en SOAPUI de manera local', done: false },
+    { title: 'Validación de certificados en el truststore.jks', done: false },
+    { title: 'Se realizo un release hasta las pruebas e2e, antes de crear el release productivo', done: false },
+    { title: 'Revisar las variables del configmap esten correctas y completas', done: false },
+    { title: 'Revisar las variables del pipeline y del release', done: false },
+    { title: 'Validación de los diferentes consumos en Postman y que estos respondan', done: false },
+    { title: 'Se generaron ya todas las OCS correspondientes', done: false },
+    { title: 'Aprobación de las OCS correspondientes', done: false },
+    { title: 'Creación de las habilitadoras', done: false },
+    { title: 'Las personas que tienen asignadas las habiltadoras si estáran durante el paso a producción', done: false },
+    { title: 'Validar que el aprobador de Creación de OC, si cuente con el tiempo y si tenga el permiso', done: false },
+    { title: 'Tener los diferentes mecanismos que permitan validar que la salidad a producción fue exitosa', done: false },
+    { title: 'Validad que los diferentes recursos de AWS estén creados', done: false }
+  ]
 });
 
 $( "ul" ).sortable();
